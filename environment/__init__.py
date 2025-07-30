9d3790e5ae1113a91a7f154955554378c4584ddc
Lindah <l.nyambura@alustudent.com>
2025-07-30 10:51:29 +0300

    local: registry added

--- environment/__init__.py
+++ environment/__init__.py
@@ -1,1 +1,7 @@
-## env registry?
+## env registry
+from gymnasium.envs.registration import register
+register(
+    id="NairobiProtestEnv-v0",
+    entry_point="environment.custom_env:NairobiCBDProtestEnv",
+    max_episode_steps=1000,
+)
